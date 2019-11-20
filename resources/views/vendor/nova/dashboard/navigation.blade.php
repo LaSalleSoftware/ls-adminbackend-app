db87d7666c8bce799244b245e1dd8cfc394f3571
Bob Bloom <bob.bloom@lasallesoftware.ca>
2019-11-20 14:45:00 -0500

    New Nova custom blade over-ride files, to suppress the display and links to the main dashboard. #24

---
+++ resources/views/vendor/nova/dashboard/navigation.blade.php
@@ -0,0 +1,15 @@
+
+{{--
+
+ Hello!
+
+ I do not want to display a Nova Dashboard.
+
+ I do not want to display a link to the Nova Dashboard.
+
+ So I created a custom view, and then blanked it out.
+
+ The original file is at vendor/laravel/nova/resources/views/dashboard/navigation.blade.php
+
+
+ --}}

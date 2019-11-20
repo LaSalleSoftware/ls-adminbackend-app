bfb329d6996ec411498be55c501decd5da81fe22
Bob Bloom <bob.bloom@lasallesoftware.ca>
2019-11-20 13:32:06 -0500

    Update Nova config for updating to Nova version 2.7.1. #24

--- config/nova.php
+++ config/nova.php
@@ -1,8 +1,9 @@
 <?php
 
+use Laravel\Nova\Actions\ActionResource;
+use Laravel\Nova\Http\Middleware\Authenticate;
 use Laravel\Nova\Http\Middleware\Authorize;
 use Laravel\Nova\Http\Middleware\BootTools;
-use Laravel\Nova\Http\Middleware\Authenticate;
 use Laravel\Nova\Http\Middleware\DispatchServingNovaEvent;
 
 return [
@@ -61,6 +62,19 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Nova Password Reset Broker
+    |--------------------------------------------------------------------------
+    |
+    | This configuration option defines the password broker that will be
+    | used when passwords are reset. This option should mirror one of
+    | the password reset options defined in the "auth" config file.
+    |
+    */
+
+    'passwords' => env('NOVA_PASSWORDS', null),
+
+    /*
+    |--------------------------------------------------------------------------
     | Nova Route Middleware
     |--------------------------------------------------------------------------
     |
@@ -90,5 +104,19 @@ return [
     */
 
     'pagination' => 'simple',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Nova Action Resource Class
+    |--------------------------------------------------------------------------
+    |
+    | This configuration option allows you to specify a custom resource class
+    | to use instead of the one that ships with Nova. You may use this to
+    | define any extra form fields or other custom behavior you need.
+    |
+    */
+    'actions' => [
+        'resource' => ActionResource::class,
+    ],
 
 ];
